2e0f1a8dc46ce22efc44f159503a07b988dcd80e
Yahya <77327200+YahyaLutta@users.noreply.github.com>
2024-04-25 12:36:13 +0300

    Files

---
+++ frontendJS/Home.js
@@ -0,0 +1,26 @@
+const ScriptButton = document.getElementById("scriptVideo")
+const BuildButton = document.getElementById("buildVideo")
+const select_btn = document.getElementById("select_btn")
+
+function ButtonClicked(Page) {
+    if (Page == `Scripting`) {
+        console.log("Opening My Scripting Portfolio. Please Wait.")
+        location.href("ScriptingPortfolio.html")
+    } else if (Page == `builds`) {
+        console.log("Opening My Building Portfolio. Please Wait.")
+        location.href("portfolio.html")
+    }
+}
+
+function ShowSelectionUi(Show) {
+
+    if (Show == true) {
+        console.log("Showing Selection Ui.");
+        document.getElementById("skillsframe").style.visibility = "visible"
+    }
+
+}
+
+ScriptButton:onclick = function(MouseEvent) {    ButtonClicked(`Scripting`)  }
+BuildButton:onclick = function(MouseEvent) {    ButtonClicked(`builds`) }
+select_btn:onclick = function(MouseEvent) {ShowSelectionUi(true)}
